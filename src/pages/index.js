4eb031aa11e6d113984782640e59fed32f6fb6bb
Florian Fuchs <florian.fuchs@bf.uzh.ch>
2018-11-06 15:23:05 +0100

    Included semantic libraries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Segment } from 'semantic-ui-react'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+import 'semantic-ui-css/semantic.min.css';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+      {/*<h1 style={{ color: `purple`, fontSize: `72px` }}>Hi there</h1>
     <p>Welcome to your new Bf Job site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/page-2/">Go to page 2</Link>*/}
+    <Segment>Beim insti zu arbeiten ist soooo cool.</Segment>
   </Layout>
+
 )
 
 export default IndexPage

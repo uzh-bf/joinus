b26939d29c752996ade94f8888c468c3843cbdb2
Manuel <manuel.keller@bf.uzh.ch>
2018-11-13 19:18:55 +0100

    redo repository with better template

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
-import Header from './header'
 import './layout.css'
+import 'semantic-ui-less/semantic.less'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,24 +17,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>

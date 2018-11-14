b54de752d9ef279ee81aefa23d399c689f11c473
Manuel <manuel.keller@bf.uzh.ch>
2018-11-14 15:45:48 +0100

    amend

--- src/components/segments/index.js
+++ src/components/segments/index.js
@@ -1,5 +1,9 @@
-import TeamSegment from "./segments/TeamSegment"
-import CitationSegment from "./segments/CitationSegment"
-import AdvantagesSegment from "./segments/AdvantagesSegment"
+import TeamSegment from "./TeamSegment"
+import CitationSegment from "./CitationSegment"
+import AdvantagesSegment from "./AdvantagesSegment"
+import ProjectSegment from "./ProjectSegment"
+import RequirementSegment from "./RequirementSegment"
+import InstituteSegment from "./InstituteSegment"
+import ApplicationFormSegment from "./ApplicationFormSegment"
 
-export { TeamSegment, CitationSegment, AdvantagesSegment }
+export { TeamSegment, CitationSegment, AdvantagesSegment, ProjectSegment, RequirementSegment, InstituteSegment, ApplicationFormSegment }

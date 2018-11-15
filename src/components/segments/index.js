7099e8d70cb9c6d86cd11425d66ae7a8dcb037f8
Manuel <manuel.keller@bf.uzh.ch>
2018-11-15 13:18:30 +0100

    Updates

--- src/components/segments/index.js
+++
@@ -1,9 +0,0 @@
-import TeamSegment from "./TeamSegment"
-import CitationSegment from "./CitationSegment"
-import AdvantagesSegment from "./AdvantagesSegment"
-import ProjectSegment from "./ProjectSegment"
-import RequirementSegment from "./RequirementSegment"
-import InstituteSegment from "./InstituteSegment"
-import ApplicationFormSegment from "./ApplicationFormSegment"
-
-export { TeamSegment, CitationSegment, AdvantagesSegment, ProjectSegment, RequirementSegment, InstituteSegment, ApplicationFormSegment }

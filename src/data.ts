938b9ff0ba08bc42ae71c3c97940c4846fa8cea2
Roland Schläfli <rolandschlaefli@gmail.com>
2020-10-24 17:02:52 +0200

    Small optimizations

--- src/data.ts
+++ src/data.ts
@@ -104,7 +104,7 @@ export const alumni = [
     study: 'Master Data Science (abgeschlossen)',
     tasklist: [
       'Weiterentwicklung MSFinance Application',
-      'Entwicklung und Wartung diverser Webapplikationen'
+      'Entwicklung und Wartung diverser Webapplikationen',
     ],
     time: 'August 2020',
   },
@@ -116,7 +116,7 @@ export const alumni = [
     tasklist: [
       'Migration bf.uzh.ch Webseite und Datenbanken',
       'Entwicklung des Management Information System 2.0',
-      'Entwicklung und Wartung diverser Webapplikationen'
+      'Entwicklung und Wartung diverser Webapplikationen',
     ],
     time: 'Februar 2020',
   },
@@ -161,7 +161,7 @@ export const technologies = [
   },
   {
     image: 'images/slideshow/kubernetes.png',
-    title: 'Kubernetes zur Orchestrierung komplexer Applikationen'
+    title: 'Kubernetes zur Orchestrierung komplexer Applikationen',
   },
   {
     image: 'images/slideshow/slim.png',
@@ -182,7 +182,7 @@ export const technologies = [
   {
     image: 'images/slideshow/typescript.png',
     title: 'TypeScript zur Verbesserung der Type-Safety',
-  }
+  },
 ]
 
 export const projects = {
@@ -191,15 +191,13 @@ export const projects = {
     title: 'Klicker',
     subtitle:
       'Open-Source Abstimmungs-Tool für Interaktivität in den Vorlesungen',
-    desc:
-      'Klicker ist ein Open-Source Instant Audience Response System - auf Deutsch ein Service, bei dem man' +
-      ' Abstimmungen erstellt und einer Audienz zum Ausfüllen übergibt und in Echtzeit Zugriff auf die Resultate hat. ' +
-      'Das Frontend wurde in React entwickelt, das Backend mit Node.js. Wichtig war von Anfang an, dass der Service ' +
-      'leicht zu bedienen ist, gut aussieht und bestens für Smartphones ausgelegt ist. So kann die grösstmögliche Anzahl ' +
-      'Studenten in den Hörsälen ermuntert werden, mitzumachen.',
-    cite:
-      'Um Klicker so interaktiv wie möglich zu machen, habe ich die Seite mit React mit einem Node.js Server ' +
-      'aufgebaut. Mit einem QR Code gelangen die Teilnehmer auf die Seite und müssen nur einmal klicken, um abzustimmen.',
+    desc: `Klicker ist ein Open-Source Instant Audience Response System - auf Deutsch ein Service, bei dem man
+      Abstimmungen erstellt und einer Audienz zum Ausfüllen übergibt und in Echtzeit Zugriff auf die Resultate hat.
+      Das Frontend wurde in React entwickelt, das Backend mit Node.js. Wichtig war von Anfang an, dass der Service
+      leicht zu bedienen ist, gut aussieht und bestens für Smartphones ausgelegt ist. So kann die grösstmögliche Anzahl
+      Studenten in den Hörsälen ermuntert werden, mitzumachen.`,
+    cite: `Um Klicker so interaktiv wie möglich zu machen, habe ich die Seite mit React mit einem Node.js Server
+      aufgebaut. Mit einem QR Code gelangen die Teilnehmer auf die Seite und müssen nur einmal klicken, um abzustimmen.`,
     citeName: 'Roli',
     tags: ['react', 'node.js', 'graphql', 'docker'],
   },
@@ -207,14 +205,12 @@ export const projects = {
     img: 'dynamics.jpg',
     title: 'Dynamics CRM',
     subtitle: 'Customizing und Automatisierungen',
-    desc:
-      'Das Weiterbildungsteam setzt für ihre Kundenverwaltung Microsoft Dynamics CRM ein. Der gesamte Teilnehmerworkflow ist darin abgebildet: ' +
-      'Von Anmeldung über Kursteilnahmen bis zu Abschlüssen sind darin alle Informationen zu jedem Kursteilnehmer erfasst.' +
-      ' Der Prozess soll möglichst automatisiert ablaufen und Fehler verhindern. Über einer neu entwickelten WebAPI kann ' +
-      "man nun auf die Daten zugreifen, sodass diese direkt auf der Webseite dargestellt werden können. Hier halten wir uns an das 'One source of truth Prinzip'.",
-    cite:
-      'Trotz Startschwierigkeiten kenne ich mich mittlerweile so gut im CRM aus, dass ich Automatisierungen schon fast schneller als unser Customizing-Partner implementieren kann. ' +
-      ' Ich bin zwar noch immer kein Fan der C# / .NET Umgebung, doch das CRM bietet uns so viel Mehrwert, dass sich die Mühe lohnt.',
+    desc: `Das Weiterbildungsteam setzt für ihre Kundenverwaltung Microsoft Dynamics CRM ein. Der gesamte Teilnehmerworkflow ist darin abgebildet:
+      Von Anmeldung über Kursteilnahmen bis zu Abschlüssen sind darin alle Informationen zu jedem Kursteilnehmer erfasst.
+      Der Prozess soll möglichst automatisiert ablaufen und Fehler verhindern. Über einer neu entwickelten WebAPI kann
+      man nun auf die Daten zugreifen, sodass diese direkt auf der Webseite dargestellt werden können. Hier halten wir uns an das "single source of truth" Prinzip'.`,
+    cite: `Trotz Startschwierigkeiten kenne ich mich mittlerweile so gut im CRM aus, dass ich Automatisierungen schon fast schneller als unser Customizing-Partner implementieren kann.
+      Ich bin zwar noch immer kein Fan der C# / .NET Umgebung, doch das CRM bietet uns so viel Mehrwert, dass sich die Mühe lohnt.`,
     citeName: 'Manuel',
     tags: ['automation', 'CRM', 'azure'],
   },
@@ -223,19 +219,17 @@ export const projects = {
     title: 'Redesign bf.uzh.ch',
     subtitle:
       'Technische Aktualisierung der Institutswebseite mit Wechsel des CMS',
-    desc:
-      'Die Universität Zürich verwendet seit langem Magnolia als Content Management System (CMS)' +
-      '. Die Webseite unseres Instituts basierte aber lange Zeit noch auf dem in Contao eingebetteten Vorgänge' +
-      'r. Im Rahmen eines umfassenden Redesigns wurde die Webseite auf Magnolia umgestellt sowie um neue Element' +
-      'e wie die Mitarbeiter- oder Vorlesungsübersicht erweitert. Wo die Möglichkeiten des CMS nicht ausreichten' +
-      ', wurden diese mit PHP und Javascript ergänzt. Im Hintergrund werden die Daten in einer von uns entwickelten MySQL Datenban' +
-      'k gespeichert, auf die wir mit einer PHP API zugreifen. Anschliessend werden die Daten visualisiert. Alle ' +
-      'Daten externer Quellen werden bei uns gecached, mit nächtlich durchlaufende Skripts werden diese aktualisiert.',
-    cite:
-      'Obwohl die neue Webseite der alten aufgrund der Corporate Identity Bestimmungen der UZH sehr ähnelt' +
-      ', ist die technische Grundlage komplett neu. Mit eigenentwickelten Komponenten versuchen wir die Nach' +
-      'teile und Einschränkungen des CMS zu umgehen und haben performante und benutzerfreundliche Zusätze eing' +
-      'epflegt. Die neue Struktur der Datenbank erlaubt uns ausserdem, viel mehr Informationen aus den Daten zu ziehen.',
+    desc: `Die Universität Zürich verwendet seit langem Magnolia als Content Management System (CMS).
+          Die Webseite unseres Instituts basierte aber lange Zeit noch auf dem in Contao eingebetteten Vorgänger.
+          Im Rahmen eines umfassenden Redesigns wurde die Webseite auf Magnolia umgestellt sowie um neue Elemente wie die Mitarbeiter-
+          oder Vorlesungsübersicht erweitert. Wo die Möglichkeiten des CMS nicht ausreichten, wurden diese mit PHP und Javascript ergänzt.
+          Im Hintergrund werden die Daten in einer von uns entwickelten MySQL Datenbank gespeichert, auf die wir mit einer PHP API zugreifen.
+          Anschliessend werden die Daten visualisiert. Alle Daten externer Quellen werden bei uns gecached,
+          mit nächtlich durchlaufende Skripts werden diese aktualisiert.`,
+    cite: `Obwohl die neue Webseite der alten aufgrund der Corporate Identity Bestimmungen der UZH sehr ähnelt,
+      ist die technische Grundlage komplett neu. Mit eigenentwickelten Komponenten versuchen wir die Nachteile
+      und Einschränkungen des CMS zu umgehen und haben performante und benutzerfreundliche Zusätze eingepflegt.
+      Die neue Struktur der Datenbank erlaubt uns ausserdem, viel mehr Informationen aus den Daten zu ziehen.`,
     citeName: 'Pascal',
     tags: ['react', 'php'],
   },
@@ -243,22 +237,19 @@ export const projects = {
     img: 'tools.jpg',
     title: 'Intranet',
     subtitle: 'Intranet mit Management-Tools',
-    desc:
-      'Unser Institut besteht aus über 50 Teams an vier verschiedenen Standorten welche im aktuellen Umfeld immer mehr auf ' +
-        'IT-Tools wie Zoom und ähnliches angewiesen sind. Deswegen ist geplant, ein Intranet zu entwickeln, wo man sich' +
-        ' über die aktuellsten Tools und deren Einsatzgebiete informieren kann. Dazu kommt, dass das Institut diverse ' +
-        'Management-Informations-Tools einsetzt, wie zum Beispiel eine eigene Mitarbeiter-Verwaltung. Auch diese Tools müssen ins Intranet überführt' +
-        ' und dort eingegliedert werden.',
-    cite:
-      '« Die Anwesenheits-Web Applikation war für mich das erste Projekt am Institut und die optimale Gelegenheit mich ' +
-      'einzuarbeiten und das Institut und seine Mitarbeiter kennenzulernen. Bei der Implementierung hatte ich ' +
-      'grosse gestalterische Freiheiten und konnte meine Web Development Skills deutlich erweitern. Es freut mich besonders,' +
-      ' dass das Endresultat nun von so vielen Mitarbeitern aktiv benutzt wird. »',
+    desc: `Unser Institut besteht aus über 50 Teams an vier verschiedenen Standorten welche im aktuellen Umfeld immer mehr auf
+      IT-Tools wie Zoom und ähnliches angewiesen sind. Deswegen ist geplant, ein Intranet zu entwickeln, wo man sich
+      über die aktuellsten Tools und deren Einsatzgebiete informieren kann. Dazu kommt, dass das Institut diverse
+      Management-Informations-Tools einsetzt, wie zum Beispiel eine eigene Mitarbeiter-Verwaltung. Auch diese Tools müssen weiterentwickelt und ins Intranet
+      eingegliedert werden.`,
+    cite: `Die Anwesenheits-Web Applikation war für mich das erste Projekt am Institut und die optimale Gelegenheit mich
+      einzuarbeiten und das Institut und seine Mitarbeiter kennenzulernen. Bei der Implementierung hatte ich
+      grosse gestalterische Freiheiten und konnte meine Web Development Skills deutlich erweitern. Es freut mich besonders,
+      dass das Endresultat nun von so vielen Mitarbeitern aktiv benutzt wird.`,
     citeName: 'Flo',
-    cite2:
-      'Das Management Information System ist ein Business Intelligence Tool. Es zieht aus verschiedenen Quellen Daten und' +
-      ' veranschaulicht die darin enthaltenen Informationen. Als Data Science Masterstudent kann ich hier mein Wissen voll nutzen ' +
-      'und so die Institutsleitung unterstützen. Daneben lerne ich viel über Visualisierungen und Web Development.',
+    cite2: `Das Management Information System ist ein Business Intelligence Tool. Es zieht aus verschiedenen Quellen Daten und
+      veranschaulicht die darin enthaltenen Informationen. Als Data Science Masterstudent kann ich hier mein Wissen voll nutzen
+      und so die Institutsleitung unterstützen. Daneben lerne ich viel über Visualisierungen und Web Development.`,
     cite2Name: 'Pascal',
     tags: ['react', 'php', 'data modelling'],
   },
@@ -267,31 +258,29 @@ export const projects = {
     title: 'eLearning',
     subtitle:
       'Digitale Lerninhalte in Form von Kursen, eSkripten, und eAssessment',
-    desc:
-      'Wer Lehrveranstaltungen von unserem Institut besucht, weiss, dass wir Innovation und Interaktivität fördern. ' +
-      'Für einige Vorlesungen bieten wir beispielsweise elektronische Skripts an, mit welchen die Studenten lernen können. ' +
-      'Wir fördern den Einsatz von eAssessment Prüfungen, stellen auf OLAT eLearning Inhalte mit einem innovativen CMS bereit und bieten den ' +
-      'Dozenten interaktive Möglichkeiten wie Klicker. Auch die Übungen während dem Semester sind digital und können ' +
-      'mit clever implementierten Excels erarbeitet werden und werden automatisiert mit Python ausgewertet.',
-    cite:
-      'Aus den Feedbacks der Studenten wissen wir, dass diese die zur Verfügung gestellten Materialien sehr schätzen. ' +
-      'Wir sind stets daran, die Inhalte auf dem aktuellsten Stand zu halten und entwickeln neue Unterstützungsmöglichkeiten.',
+    desc: `Wer Lehrveranstaltungen von unserem Institut besucht, weiss, dass wir Innovation und Interaktivität fördern.
+      Für einige Vorlesungen bieten wir beispielsweise elektronische Skripts an, mit welchen die Studenten lernen können.
+      Wir fördern den Einsatz von eAssessment Prüfungen, stellen auf OLAT eLearning Inhalte mit einem innovativen CMS bereit und bieten den
+      Dozenten interaktive Möglichkeiten wie Klicker. Auch die Übungen während dem Semester sind digital und können
+      mit clever implementierten Excels erarbeitet werden und werden automatisiert mit Python ausgewertet.`,
+    cite: `Aus den Feedbacks der Studenten wissen wir, dass diese die zur Verfügung gestellten Materialien sehr schätzen.
+      Wir sind stets daran, die Inhalte auf dem aktuellsten Stand zu halten und entwickeln neue Unterstützungsmöglichkeiten.`,
     citeName: 'Christian',
-    cite2:
-      'Unser neu entwickeltes Strapi Kurs-CMS erlaubt es jedem, Änderungen an Kursinhalten vorzunehmen, ohne sich mit HTML auszukennen. ' +
-      'Die Inhalte werden danach als React.js Applikation exportiert und in ein LMS wie OLAT eingebunden.',
+    cite2: `Unser neu entwickeltes Strapi Kurs-CMS erlaubt es jedem, Änderungen an Kursinhalten vorzunehmen, ohne sich mit HTML auszukennen.
+      Die Inhalte werden danach als React.js Applikation exportiert und in ein LMS wie OLAT eingebunden.`,
     cite2Name: 'Roli',
     tags: ['react', 'strapi', 'lms', 'python'],
   },
   jobeye: {
-    img: 'jobeye.jpg',
+    img: 'pfm_game.png',
     title: 'Game-Based Learning',
-    subtitle:
-      'Unterstützung der Lehre durch Simulationen und Serious Games',
-    desc: `Am IBF bieten wir mehrere Vera`,
-    cite:
-      'Unsere Simulationen erlauben es den Studierenden, theoretische Konzepte spielerisch anzuwenden. ' +
-      '...',
+    subtitle: 'Unterstützung der Lehre durch Simulationen und Serious Games',
+    desc: `Am IBF bieten wir mehrere Veranstaltungen und Seminare an, die durch Simulationen unterstützt werden.
+          Die Studierenden übernehmen zum Beispiel die Rolle einer Bank und müssen diese über mehrere Perioden bestmöglich führen.
+          Verschiedenen Teams ("Banken") spielen gegen- bzw. miteinander auf demselben Markt und können sich durch Kundenzufriedenheit,
+          Performance, und weitere Faktoren zum Marktführer entwickeln.`,
+    cite: `Unsere Simulationen und Serious Games erlauben es den Studierenden, theoretische Konzepte spielerisch anzuwenden.
+      Diese Art des Lernens kommt immer sehr gut an und macht es macht viel Spass, dabei mitzuwirken.`,
     citeName: 'Roli',
     tags: ['react', 'python', 'gamification'],
   },
@@ -325,29 +314,26 @@ export const teams = [
     imageSrc: 'images/lisa.jpg',
     leaderName: 'Lisa Liechti',
     leaderPosition: 'Leitung Kommunikation IBF',
-    teamDescription:
-      'Bei mir stehen viele Data Science Projekte an, bei welchen ich eure ' +
-      'Unterstützung brauche. Dazu kommen immer wieder Aufgaben im Web-Design und der Gestaltung von ' +
-      'Magazinen und Newslettern. Ich suche also kreative Data Scientists!',
+    teamDescription: `Bei mir stehen viele Data Science Projekte an, bei welchen ich eure
+      Unterstützung brauche. Dazu kommen immer wieder Aufgaben im Web-Design und der Gestaltung von
+      Magazinen und Newslettern. Ich suche also kreative Data Scientists!`,
   },
   {
     teamName: 'Teaching Center',
     imageSrc: 'images/johanna.jpg',
     leaderName: 'Johanna Braun',
     leaderPosition: 'Leitung Teaching Center IBF',
-    teamDescription:
-      'Wir wollen die Lehrveranstaltungen unseres Instituts innovativ und interaktiv ' +
-      'gestalten. Deshalb brauchen wir IT-Support für die digital unterstützten Kurse, für Klicker und ' +
-      'Jobeye oder eine neue Idee von dir!',
+    teamDescription: `Wir wollen die Lehrveranstaltungen unseres Instituts innovativ und interaktiv
+      gestalten. Deshalb brauchen wir IT-Support für die digital unterstützten Kurse, für Klicker und
+      Jobeye oder eine neue Idee von dir!`,
   },
   {
     teamName: 'Weiterbildung',
     imageSrc: 'images/beni.jpg',
     leaderName: 'Benjamin Wilding',
-    leaderPosition: 'Geschäftsführer, Leitung Lehre & Weiterbildung',
-    teamDescription:
-      'Als Weiterbildungsanbieter brauchen wir kreative Informatiker*innen, die die User ' +
-      'Experience mit innovativen Tools verbessern und die Kundenverwaltung mit ' +
-      'Automatisierungen effizienter gestalten.',
+    leaderPosition: 'Geschäftsführer IBF, Leitung Lehre & Weiterbildung',
+    teamDescription: `Als Weiterbildungsanbieter brauchen wir kreative Informatiker*innen, die die User
+      Experience mit innovativen Tools verbessern und die Kundenverwaltung mit
+      Automatisierungen effizienter gestalten.`,
   },
 ]

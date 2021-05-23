ee70428901560b5a007464a518319736f7fbd7fa
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-23 19:33:28 +0200

    chore: remove storybook

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  presets: [path.resolve(__dirname, './next-preset.js')],
-}
